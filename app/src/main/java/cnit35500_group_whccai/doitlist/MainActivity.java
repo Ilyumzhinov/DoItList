88b6e75d21584985d7f72798bf11a094a1974bba
Artem <ilumzhinov@gmail.com>
2018-10-28 18:15:04 -0400

    Initial commit

---
+++ app/src/main/java/cnit35500_group_whccai/doitlist/MainActivity.java
@@ -0,0 +1,15 @@
+package cnit35500_group_whccai.doitlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
