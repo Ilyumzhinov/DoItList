94a8a6593f92457f7ae8dcea4e2acd755432418a
irisHuang <huang814@purdue.edu>
2018-11-04 01:21:58 -0400

    11/4/18 layout

--- app/src/main/java/cnit35500_group_whccai/doitlist/MainActivity.java
+++ app/src/main/java/cnit35500_group_whccai/doitlist/MainActivity.java
@@ -1,7 +1,9 @@
 package cnit35500_group_whccai.doitlist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void NewTask(View view) {
+        Intent i = new Intent(this,TaskActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/cnit35500_group_whccai/doitlist/TaskActivity.java
@@ -0,0 +1,41 @@
+package cnit35500_group_whccai.doitlist;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+public class TaskActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+
+        drawerLayout = findViewById(R.id.taskLayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void saveTask(View view) {
+    }
+
+    public void highlight(View view) {
+    }
+}
