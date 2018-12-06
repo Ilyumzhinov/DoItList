86658fc3a11d7852b5a58ea7d394c174d7eefde2
sccunningham <cunnin57@purdue.edu>
2018-12-05 23:24:10 -0500

    Added global time functions to format things appropriately. Changed some layout text

--- app/src/main/java/Functional/Globals.java
+++ app/src/main/java/Functional/Globals.java
@@ -1,9 +1,13 @@
 package Functional;
 
 import android.app.Activity;
+import android.text.format.DateUtils;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Globals
 {
@@ -50,8 +54,21 @@ public class Globals
     // E.g.: 110 -> “1 h, 50 min”
     public static String formatTimeTotal(Long xMinutes)
     {
-        // PLACEHOLDER LOGIC
-        return String.valueOf(xMinutes) + " min";
+        String returnMinutes;
+        if (xMinutes >= 1440) {
+            // If time is longer than a day
+            returnMinutes = xMinutes/24/60 + " D, " + xMinutes/60%24 + " H, " + xMinutes%60 + " min";
+        } else if (xMinutes < 60) {
+            // If time is shorter than a day
+           returnMinutes = xMinutes%60 + " min";
+        } else {
+            returnMinutes = xMinutes/60%24 + " H, " + xMinutes%60 + " min";
+        }
+        if (returnMinutes.contains("-")) {
+            returnMinutes = returnMinutes.replaceAll("-", "");
+            returnMinutes += " ago";
+        }
+        return returnMinutes;
     }
 
     // Receive a date and parse it into a string of that date or one of special cases, like “Today”, “Tomorrow”, “Yesterday”.
@@ -60,10 +77,26 @@ public class Globals
     public static String formatDate(LocalDateTime xLocalDate)
     {
         // Reference: https://stackoverflow.com/questions/28177370/how-to-format-localdate-to-string
-        // PLACEHOLDER LOGIC
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy, HH:mm");
-        String formattedString = xLocalDate.format(formatter);
-
+        // Grab local time
+        Date currentTime = Calendar.getInstance().getTime();
+        // Convert it to localdatetime so everything is the same
+        LocalDateTime localCurrentTime = currentTime.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        DateTimeFormatter formatter;
+        String formattedString;
+        // compare dates to see if its today, tomorrow, or yesterday
+        if (xLocalDate.getDayOfYear()-1 == localCurrentTime.getDayOfYear()) {
+            formatter = DateTimeFormatter.ofPattern("HH:mm");
+            formattedString = "Tomorrow, " + xLocalDate.format(formatter);
+        } else if (xLocalDate.getDayOfYear() == localCurrentTime.getDayOfYear()) {
+            formatter = DateTimeFormatter.ofPattern("HH:mm");
+            formattedString = "Today, " + xLocalDate.format(formatter);
+        } else if (xLocalDate.getDayOfYear()+1 == localCurrentTime.getDayOfYear()) {
+            formatter = DateTimeFormatter.ofPattern("HH:mm");
+            formattedString = "Yesterday, " + xLocalDate.format(formatter);
+        } else {
+            formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy, HH:mm");
+            formattedString = xLocalDate.format(formatter);
+        }
         return formattedString;
     }
 }
